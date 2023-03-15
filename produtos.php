026e0669b1a127c4e1b54007846777998f085203
Tamires Bretz <tamires.bretz@alunos.ifsuldeminas.edu.br>
2023-03-15 11:06:34 -0300

    aula9

---
+++ produtos.php
@@ -0,0 +1,24 @@
+<?php
+//vetores menores
+  $produto1 = [
+                "nome" => "Câmera",
+                "marca" => "LG",
+                "preco" => 2500
+  ];
+  $produto2 = [
+                "nome" => "Notebook",
+                "marca" => "Dell",
+                "preco" => 7500
+  ];
+  $produto3 = [
+                "nome" => "Smartphone",
+                "marca" => "Samsung",
+                "preco" => 3500
+  ]; 
+
+  //vetor maior, que armazena os outros vetores
+  $produtos = [
+                $produto1,
+                $produto2,
+                $produto3
+  ]; 
